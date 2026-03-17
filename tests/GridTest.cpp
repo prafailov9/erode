87ffef4535142323a6843bf2ef3719f49d7344ef
Plamen Rafailov <prafailov9@gmail.com>
2026-03-17 12:50:21 +0200

    added tests and fixed cmake

---
+++ tests/GridTest.cpp
@@ -0,0 +1,8 @@
+#include "Grid.h"
+#include <gtest/gtest.h>
+
+TEST(GridTest, InitializesCorrectly) {
+  Grid grid(10, 10);
+  //   EXPECT_EQ(grid.width(), 10);
+  //   EXPECT_EQ(grid.height(), 10);
+}
